6dfdf376a2b05d1839ef15125dd1188ae7fe35f1
Elio <100736466+Eliomatheus@users.noreply.github.com>
2023-05-19 11:01:59 -0300

    Update 1_visao_empresa.py
    Atualizando versão do código do pandas

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -259,9 +259,9 @@ st.sidebar.markdown( """___""" )
 st.sidebar.markdown( '## Selecione uma data limite ' )
 date_slider = st.sidebar.slider(
     'Até qual valor?',
-    value=pd.datetime( 2022, 4, 13 ),
-    min_value=pd.datetime( 2022, 2, 11 ),
-    max_value=pd.datetime( 2022, 4, 6 ),
+    value=pd.Timestamp( 2022, 4, 13 ),
+    min_value=pd.Timestamp( 2022, 2, 11 ),
+    max_value=pd.Timestamp( 2022, 4, 6 ),
     format='DD-MM-YYYY' )
 
 
